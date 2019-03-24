b88891c3f58abb987344b33bb5665f16343ac055
Vadim Opolski <mathmatheart@gmail.com>
2019-03-24 09:12:51 +0300

    Initial commit

---
+++ src/main/java/com/example/ironworkman/IronWorkManApplication.java
@@ -0,0 +1,13 @@
+package com.example.ironworkman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronWorkManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronWorkManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ironworkman/IronWorkManApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ironworkman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IronWorkManApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
